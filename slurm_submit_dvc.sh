6a6008f73e39fb2c568c35115f482d74551efe77
Evan Komp <ekomp@kd5.head.cm.kestrel.hpc.nrel.gov>
2024-05-08 16:40:46 -0600

    codecarbon set up

---
+++ slurm_submit_dvc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --partition=standard
+#SBATCH --account=bpms
+#SBATCH --job-name=dvc_stage
+#SBATCH --output=dvc_stage.out
+#SBATCH --nodes=1
+#SBATCH --mem=100GB              # memory per node
+#SBATCH --time=0-0:30            # Max time (DD-HH:MM)
+
+############### CARBON TRACKING
+# start codecarbon
+PID=$(/projects/bpms/ekomp_tmp/software/carbon/start_tracker.sh)
+echo main
+echo $PID
+# Save its PID
+
+# Define a cleanup function
+cleanup() {
+    echo "Cleaning up..."
+    kill -SIGINT $PID
+    sleep 10
+}
+# # Set the trap
+trap cleanup EXIT
+#################### END CARBON TRACKING
+dvc repro -s $1 --force
