90dd724966e4955d980b93c89c7f9beb0b04b486
Evan Komp <evan.komp@nrel.gov>
2024-06-13 16:35:02 -0600

    rewrote first step to run ev couplings jackhmmer pipeline

--- aide_predict/utils/common.py
+++ aide_predict/utils/common.py
@@ -11,6 +11,8 @@ Common utility functions
 from aide_predict.utils.constants import AA_ALPHABET
 
 from typing import Iterable
+from types import SimpleNamespace
+
 
 def process_amino_acid_sequences(sequences: Iterable[str]) -> Iterable[str]:
     """Processes a list of amino acid sequences to ensure that they are valid.
@@ -28,3 +30,18 @@ def process_amino_acid_sequences(sequences: Iterable[str]) -> Iterable[str]:
             raise ValueError(f"Invalid amino acid in sequence: {seq}")
         yield seq.upper()
     
+
+def convert_dvc_params(dvc_params_dict: dict):
+    """DVC Creates a nested dict with the parameters.
+    
+    We want an object that has nested attributes so that we can
+    access parameters with dot notation.
+    """
+    def _dict_to_obj(d):
+        if isinstance(d, dict):
+            return SimpleNamespace(**{k: _dict_to_obj(v) for k, v in d.items()})
+        elif isinstance(d, list):
+            return [_dict_to_obj(x) for x in d]
+        else:
+            return d
+    return _dict_to_obj(dvc_params_dict)
