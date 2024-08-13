25b71dc6df4edc24083859483744af28942a1efd
Evan Komp <evan.komp@nrel.gov>
2024-08-13 16:11:14 -0600

    aquisition functions out of scope for now

--- aide_predict/aquisition_functions/aquisition_function.py
+++
@@ -1,6 +0,0 @@
-# aide_predict/aquisition_functions/aquisition_function.py
-'''
-* Author: Evan Komp
-* Created: 5/7/2024
-* (c) Copyright by Bottle Institute @ National Renewable Energy Lab, Bioeneergy Science and Technology
-'''
